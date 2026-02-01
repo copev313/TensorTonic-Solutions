034999f080d54e583b531cfaad4e3836755a9a1a
Evan Cope <59665246+copev313@users.noreply.github.com>
2026-01-31 19:08:06 -0500

    Add solution for covariance-matrix

---
+++ covariance-matrix/covariance-matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def covariance_matrix(X):
+    """Compute covariance matrix from dataset X.
+    
+    Parameters
+    ----------
+    X: list[list[float]] | np.ndarray
+        Dataset with shape (N, D)
+    
+    Returns
+    -------
+    np.ndarray | None
+        Covariance matrix with shape (D, D). None for invalid input (N < 2 or not 2D)
+    """
+    X = np.asarray(X, dtype=float)
+    # [CASE] Invalid input dims:
+    if X.ndim != 2:
+        return None
+
+    N = X.shape[0]
+    if N < 2:
+        return None
+
+    mu = np.mean(X, axis=0)
+    X_centered = X - mu
+    cov_mat = (X_centered.T @ X_centered) / (N - 1)
+    return cov_mat
