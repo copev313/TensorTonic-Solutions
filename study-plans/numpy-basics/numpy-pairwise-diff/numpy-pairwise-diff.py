5c27e52ea10329830d473a0c959159fa18c450d8
Evan Cope <59665246+copev313@users.noreply.github.com>
2026-04-27 10:43:24 -0400

    Add solution for numpy-pairwise-diff

---
+++ study-plans/numpy-basics/numpy-pairwise-diff/numpy-pairwise-diff.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def pairwise_diff(a):
+    """Returns: np.ndarray of shape (n, n) where out[i,j] = a[i] - a[j]"""
+    arr = np.array(a, dtype=np.float64)
+    col = arr[:, np.newaxis].copy()
+    return col - arr
+    
